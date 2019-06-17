8bd2179fe5084546454ab3af94571c32502d00d4
nag-training <nag-training@172.20.10.2>
2019-06-17 15:23:32 +0530

    FP in java

--- 12-FP-in-java/src/com/How_To_Write_Functions_In_Java.java
+++ 12-FP-in-java/src/com/How_To_Write_Functions_In_Java.java
@@ -4,6 +4,27 @@ import java.util.function.Predicate;
 
 import com.model.Product;
 
+// using 'Functional Interface '
+
+// i.e type of java function is 'Functional Interface'
+
+// FI ==> any interface with 'single-abstract-method' ( sam )
+
+/*
+ * 
+ *  syntax of function
+ *  
+ *  
+ * 	FI v=param(s)-> body             -- Lambda Expression
+ * 
+ * 		 p1      -> expression
+ *       p1      -> {expression(s)}
+ *      (p1,p2)  -> {expression(s)}
+ *       
+ * 	
+ */       
+
+// e.g
 @FunctionalInterface
 interface StringToIntMapper {
 	int map(String s);
